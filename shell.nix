6094379d90b68e7810e475b53c1fc113f7004137
harris-chris <chrisharriscjh@gmail.com>
2021-10-07 11:40:16 +0900

    Clash shell working

---
+++ shell.nix
@@ -0,0 +1,8 @@
+(import ./default.nix).shellFor {
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "latest";
+    #haskell-language-server = "latest";
+  };
+  #inputsFrom = [ ./clash-only-sell.nix ];
+}
